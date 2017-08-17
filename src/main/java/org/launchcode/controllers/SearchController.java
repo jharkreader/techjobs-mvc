c9077ec9e66b5c21ed810b6fca38c337a3477124
Jennifer Harkreader <jen3072@gmail.com>
2017-08-16 18:41:36 -0700

    Typo fixing and testing

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -24,7 +24,7 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
-    @RequestMapping(value = "/results")
+    @RequestMapping(value = "results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<HashMap<String, String>> jobs;
 
